74c93a783556048293baa9b3bd4720d997ebc7ef
Rakibulislam1 <rirakib03@gmail.com>
2023-11-29 19:20:12 +0600

    all user page added

---
+++ src/Components/AllUser/AllUser.jsx
@@ -0,0 +1,185 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import Swal from "sweetalert2";
+import { useState } from "react";
+
+const AllUser = () => {
+
+  const axiosPublic = useAxiosPublic();
+  const { data: users = [], refetch } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/users");
+      console.log(res.data);
+      return res.data; // Add this line to return the data
+    },
+  });
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 2;
+
+  const indexOfLastUser = (currentPage + 1) * itemsPerPage;
+  const indexOfFirstUser = indexOfLastUser - itemsPerPage;
+  const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
+
+  const handleMakeAdmin = async (user) => {
+    const result = await axiosPublic.patch(`/users/role/${user._id}`);
+    console.log(result);
+    if (result.data.modifiedCount > 0) {
+      refetch();
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: `${user.name} is Admin Now`,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+
+  return (
+    <div>
+      <table className="min-w-full divide-y divide-gray-200 overflow-x-auto">
+        <thead className="bg-gray-50">
+          <tr>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Profile Image
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Name
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Email
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {currentUsers?.map((user) => (
+            <>
+              <tr key={user._id}>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0 h-10 w-10">
+                      <img
+                        className="h-10 w-10 rounded-full"
+                        src={user?.image}
+                        alt=""
+                      />
+                    </div>
+                    <div className="ml-4">
+                      <div className="text-sm font-medium text-gray-900"></div>
+                      <div className="text-sm text-gray-500"></div>
+                    </div>
+                  </div>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="text-sm text-gray-900">{user?.name}</div>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                    {user?.email}
+                  </span>
+                </td>
+
+                <td className="px-6  py-4 whitespace-nowrap text-sm text-gray-500">
+                  {user?.role === "Admin" ? (
+                    <p>Admin</p>
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      className="text-indigo-600 hover:text-indigo-900"
+                    >
+                      Make Admin
+                    </button>
+                  )}
+                </td>
+              </tr>
+            </>
+          ))}
+        </tbody>
+      </table>
+
+      
+        {/* Pagination */}
+        <nav className="flex items-center justify-center pt-20 -space-x-px">
+          <button
+            type="button"
+            className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-gray-700 dark:hover:bg-white/10 dark:focus:bg-white/10"
+            onClick={() => setCurrentPage((prev) => prev - 1)}
+            disabled={currentPage === 0}
+          >
+            <svg
+              className="flex-shrink-0 w-3.5 h-3.5"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m15 18-6-6 6-6" />
+            </svg>
+            <span className="hidden sm:block">Previous</span>
+          </button>
+          {[...Array(Math.ceil(users.length / itemsPerPage)).keys()].map((page) => (
+            <button
+              key={page}
+              type="button"
+              className={`min-h-[38px] min-w-[38px] flex justify-center items-center ${
+                currentPage === page
+                  ? 'bg-gray-200 text-gray-800'
+                  : 'border border-gray-200 text-gray-800 hover:bg-gray-100'
+              } py-2 px-3 text-sm first:rounded-s-lg last:rounded-e-lg focus:outline-none focus:bg-gray-300 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-600 dark:border-gray-700 dark:text-white dark:focus:bg-gray-500`}
+              onClick={() => setCurrentPage(page)}
+            >
+              {page + 1}
+            </button>
+          ))}
+          <button
+            type="button"
+            className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-gray-700 dark:hover:bg-white/10 dark:focus:bg-white/10"
+            onClick={() => setCurrentPage((prev) => prev + 1)}
+            disabled={indexOfLastUser >= users.length}
+          >
+            <span className="hidden sm:block">Next</span>
+            <svg
+              className="flex-shrink-0 w-3.5 h-3.5"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m9 18 6-6-6-6" />
+            </svg>
+          </button>
+        </nav>
+        {/* End Pagination */}
+    </div>
+    
+  );
+};
+
+export default AllUser;
